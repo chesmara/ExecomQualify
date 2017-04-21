28641e6a11b021058948377875233e9e5716d7be
Chesmara <srdjan@ekoneimar.com>
2017-04-21 20:21:23 +0200

    toolbar i unos nove liste

--- app/src/main/java/com/example/chesmara/execomqualify/activities/MainActivity.java
+++ app/src/main/java/com/example/chesmara/execomqualify/activities/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.chesmara.execomqualify.activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -54,15 +59,100 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case(R.id.add_new_list):
+                final Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.add_new_list);
+
+                Button add = (Button) dialog.findViewById(R.id.add_list);
+                add.setOnClickListener(new View.OnClickListener()  {
+                    @Override
+                    public void onClick(View v){
+                        EditText listName = (EditText) dialog.findViewById(R.id.new_list_name);
+
+                        ShopList sl= new ShopList();
+                        sl.setmName(listName.getText().toString());
+
+
+                        try {
+                            getDatabaseHelper().getmShopListDao().create(sl);
+
+                            dialog.dismiss();
+                            refresh();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+               });
+
+                Button cancel = (Button) dialog.findViewById(R.id.add_list_cancel);
+                cancel.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+                dialog.show();
+                break;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void refresh(){
+
+        ListView listview = (ListView) findViewById(R.id.shoping_lists);
+
+        if (listview != null){
+            ArrayAdapter<ShopList> adapter= (ArrayAdapter<ShopList>) listview.getAdapter();
+
+            if(adapter !=null){
+
+
+                try {
+
+                    adapter.clear();
+
+                    List<ShopList> list = null;
+                    list = getDatabaseHelper().getmShopListDao().queryForAll();
+
+                    adapter.addAll(list);
+                    adapter.notifyDataSetChanged();
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
 
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-
-
-
+        refresh();
+    }
 
     public DatabaseHelper getDatabaseHelper() {
         if (databaseHelper == null) {
